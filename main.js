6b40e95b5fe2e8cfaec47aefc9755c26b8bffc80
Ectomorph <ectomorph.js@gmail.com>
2022-06-20 07:39:32 +0300

    commit

--- main.js
+++ main.js
@@ -2,10 +2,8 @@
 
 let menuButton = document.querySelector('.menu-button');
 let navBlock = document.querySelector('nav');
-let adressBlock = document.querySelector('.adress-block');
 
 menuButton.addEventListener('click', (e) => {
     menuButton.classList.toggle('active');
     navBlock.classList.toggle('active');
-    adressBlock.classList.toggle('active');
 });
