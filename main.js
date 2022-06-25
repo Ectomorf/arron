32383890c968818ae0a8f49e472bc32f6326b654
Ectomorph <ectomorph.js@gmail.com>
2022-06-25 08:36:57 +0300

    commit

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 //hamburger menu//
-
-let menuButton = document.querySelector('.menu-button');
-let navBlock = document.querySelector('nav');
-
-menuButton.addEventListener('click', (e) => {
-    menuButton.classList.toggle('active');
-    navBlock.classList.toggle('active');
-});
