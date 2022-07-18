ccda96ba98d26f292c711ea9da0a8423dc4d8e66
ectomorph <ectomorph.js@gmail.com>
2022-07-18 22:10:01 +0300

    adaptive header to 768 and burger menu

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 /* hamburger menu */
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('nav');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('active');
+    menu.classList.toggle('active');
+});
